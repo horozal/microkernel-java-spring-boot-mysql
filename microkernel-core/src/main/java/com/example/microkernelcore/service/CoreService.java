76e3c5e4f5ef5cc93973e738fa63a570648cc6d6
Feryal Fulya Horozal <horozal@atb-bremen.de>
2023-06-16 18:03:09 +0200

    Add file

---
+++ microkernel-core/src/main/java/com/example/microkernelcore/service/CoreService.java
@@ -0,0 +1,5 @@
+package com.example.microkernelcore.controller;
+
+public class CoreController {
+
+}
---
+++ microkernel-plugin/src/main/java/com/example/microkernelplugin/service/PluginService.java
@@ -0,0 +1,5 @@
+package com.example.microkernelplugin.service;
+
+public class PluginService {
+
+}
